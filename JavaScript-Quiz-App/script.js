28a94e6ca3aaeccb4403295d149c329b89f34a6b
Yakovdaza <91088305+Yakovdaza@users.noreply.github.com>
2022-02-16 23:23:11 -0500

    juego

---
+++ JavaScript-Quiz-App/script.js
@@ -0,0 +1,112 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'How much is overdraft fee?',
+    answers: [
+      { text: '36', correct: true },
+      { text: '38', correct: false },
+      { text: '32', correct: false },
+      { text: '35', correct: false }
+    ]
+  },
+  {
+    question: 'how long is low cast mode?',
+    answers: [
+      { text: '72-90 hours', correct: false },
+      { text: '5 -8 hours', correct: false },
+      { text: '12-15 hours ', correct: false},
+      { text: '24-27 hours', correct:true  }
+    ]
+  },
+  {
+    question: 'Am I automatically covered by overdraft coverage or protection?',
+    answers: [
+      { text: 'no', correct: true },
+      { text: 'YES!!!', correct: false }
+    ]
+  },
+  {
+    question: 'what PNC feature let you dodge overdraft fee?',
+    answers: [
+      { text: 'low cash mode and overdarft protection', correct: false },
+      { text: 'overdraft coverage', correct: true }
+    ]
+  }
+]
